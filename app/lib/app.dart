719636a2daef1886c1fc1cf549d82f2d6eb29a19
koukiito <dev.koukiito@gmail.com>
2024-04-05 08:48:41 +0900

    update: support localization
    - add localizations support

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'package:repo_viewer/ui/route/app_router.dart';
 
 class App extends HookConsumerWidget {
@@ -8,6 +10,8 @@ class App extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
+      localizationsDelegates: L10n.localizationsDelegates,
+      supportedLocales: L10n.supportedLocales,
       debugShowCheckedModeBanner: false,
       routerConfig: ref.watch(routerProvider),
       title: 'Repo Viewer',
